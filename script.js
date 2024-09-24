75ab375add4865d8c19022802ebcb1b1cd737852
Shohruh <151940381+sattorovshoxrux3009@users.noreply.github.com>
2024-09-24 23:13:22 +0500

    Update script.js
    K

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weddingDate = new Date('November 1, 2024 19:00:00').getTime();
+const weddingDate = new Date('November 1, 2024 18:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
