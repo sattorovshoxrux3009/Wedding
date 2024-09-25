c09c5d1bcf7fde98a39a9a72c1f777b82da3fe5b
Shohruh <sattorovshoxrux3009@gmail.com>
2024-09-25 17:25:17 +0500

    Create

--- script.js
+++ script.js
@@ -34,3 +34,7 @@ function togglePlayPause() {
         playPauseBtn.textContent = 'Play';
     }
 }
+setTimeout(function() {
+    const video = document.getElementById('myVideo');
+    video.play();
+  }, 1000)
